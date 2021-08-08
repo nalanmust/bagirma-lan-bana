c3a31a0bc9b4a26ba366c4e1264e3982f81c7eb9
nalanmust <88629253+nalanmust@users.noreply.github.com>
2021-08-08 22:28:25 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 from flask import Flask, render_template
+from telegram_bot import TelethonBot
+import asyncio
 
 
 app = Flask(__name__)
+bot = TelethonBot(3226017, "8702e11ddbf002c90b373da426332147")
+loop = asyncio.get_event_loop()
 
 
 @app.route("/")
@@ -9,5 +13,18 @@ def mainpage() -> str:
     return "sa"
 
 
+@app.route("/api/<telegram_username>")
+def api_page(telegram_username: str):
+    info = loop.run_until_complete(
+        bot.get_user_info(
+            telegram_username
+        )
+    )
+
+    return render_template(
+        "svg.html", info=info
+    )
+
+
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0')
