c7a9b1bd3328649cdf45dd52c68174e34f0114ec
nalanmust <88629253+nalanmust@users.noreply.github.com>
2021-08-08 22:27:53 +0300

    Create telegram_bot.py

---
+++ telegram_bot.py
@@ -0,0 +1,23 @@
+from telethon import TelegramClient
+from telethon.tl.functions.users import GetFullUserRequest
+
+
+class TelethonBot():
+    def __init__(self, api_id: int, api_hash: str) -> None:
+        self.client = TelegramClient(
+            r'hkeybot', api_id, api_hash
+        )
+        self.client.start()
+
+
+    async def get_user_info(self, username: str) -> str:
+        user = await self.client(GetFullUserRequest(username))
+        info =  {
+            "firstname": user.user.first_name,
+            "pp_path": await self.client.download_profile_photo(username, file="static"),
+            "bio": user.about
+        }
+
+        info['pp_path'] = info['pp_path'].replace("\\", "/")
+
+        return info
